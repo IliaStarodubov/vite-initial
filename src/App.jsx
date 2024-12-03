5f3f246a1c25553104b956def1168c65484be750
IliaStarodubov <m.telo@bk.ru>
2024-12-03 21:29:55 +0500

    without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
 // import reactLogo from './assets/react.svg';
 import ReactLogo from './assets/react.svg?react';
 import './App.css';
+import React from 'react';
 
 const date = new Date();
-
-export const App = () => (
-	<div className="App">
-		<header className="App-header">
-			<p>
-				Edit <code>src/App.js</code> and save to reload .
-			</p>
-			<h1>Vite + React</h1>
-			{/* <img src={reactLogo} alt="react Logo" /> */}
-			<ReactLogo className="logo" /> {/* Декларативный стиль-используется JSX */}
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-			<p>{date.getFullYear()}</p>
-			{/* Декларативный стиль-используется JSX */}
-		</header>
-	</div>
-);
+export const App = () =>
+	/*#__PURE__*/ React.createElement(
+		// Императивный стиль
+		'div',
+		{
+			className: 'App',
+		},
+		/*#__PURE__*/ React.createElement(
+			// Императивный стиль
+			'header',
+			{
+				className: 'App-header',
+			},
+			/*#__PURE__*/ React.createElement(
+				// Императивный стиль
+				'p',
+				null,
+				'Edit ',
+				/*#__PURE__*/ React.createElement('code', null, 'src/App.js'), // Императивный стиль
+				' and save to reload .',
+			),
+			/*#__PURE__*/ React.createElement('h1', null, 'Vite + React'), // Императивный стиль
+			/*#__PURE__*/ React.createElement(ReactLogo, {
+				// Императивный стиль
+				className: 'logo',
+			}),
+			' ',
+			/*#__PURE__*/ React.createElement(
+				// Императивный стиль
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			/*#__PURE__*/ React.createElement('p', null, date.getFullYear()), // Императивный стиль
+		),
+	);
